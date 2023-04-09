acb4233c4fc53b7b598aeb326b4ecc8a7ec1f22e
yuri_sn <yuri.snitko@gmail.com>
2023-04-09 15:25:32 +0300

    memory game

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ const endGame = document.querySelector('span');
 let openCards = [];
 let matchedCards = [];
 let cardsArray = [];
+let attempsArray = [];
 
 function generateTable(n, k) {
   const table = document.createElement('table');
@@ -39,10 +40,12 @@ function onClick(event) {
         }
       } else {
         setTimeout(hideCards, 0.3);
+        attempsArray.push(...openCards);
+        const attempsCount = attempsArray.length / 2;
+        attemps.value = attempsCount;
       }
     }
   }
-  // console.log(target);
 }
 
 function insertTable(table) {
@@ -61,25 +64,25 @@ function hideCards() {
   openCards.length = 0;
 }
 
-function fillCellsEasy() {
+function fillCellsEasy(cards) {
   let easy = cards.slice(0, 8);
   cardsArray = [...easy, ...easy];
   return cardsArray;
 }
 
-function fillCellsMedium() {
+function fillCellsMedium(cards) {
   let medium = cards.slice(0, 10);
   cardsArray = [...medium, ...medium];
   return cardsArray;
 }
 
-function fillCellsHard() {
+function fillCellsHard(cards) {
   let hardcore = cards.slice(0, 12);
   cardsArray = [...hardcore, ...hardcore];
   return cardsArray;
 }
 
-function fillCells() {
+function fillCells(cardsArray) {
   const tableTd = document.querySelectorAll('td');
   let i = 0;
   cardsArray.sort(() => Math.random() - 0.5);
@@ -89,22 +92,21 @@ function fillCells() {
       i++
     )
   );
-  console.log(cardsArray);
 }
 
 function setDifficultyLevel(level) {
   switch (level) {
     case 'easy':
       insertTable(generateTable(4, 4));
-      fillCells(fillCellsEasy());
+      fillCells(fillCellsEasy(cards));
       break;
     case 'medium':
       insertTable(generateTable(4, 5));
-      fillCells(fillCellsMedium());
+      fillCells(fillCellsMedium(cards));
       break;
     case 'hardcore':
       insertTable(generateTable(4, 6));
-      fillCells(fillCellsHard());
+      fillCells(fillCellsHard(cards));
       break;
   }
 }
@@ -112,6 +114,8 @@ function setDifficultyLevel(level) {
 function resetGame() {
   matchedCards = [];
   openCards = [];
+  attempsArray = [];
   matches.value = 0;
+  attemps.value = 0;
   endGame.textContent = '';
 }
